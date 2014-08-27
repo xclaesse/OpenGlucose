5620184c14bcef2aeec02826c03984c085d31b39
Xavier Claessens <xclaesse@gmail.com>
2014-08-27 17:51:22 -0400

    Move InsuLinx data into its own directory
    Also slightly improse the parser and add more logs
    captured from the windows application

---
+++ data/insulinx/parser.py
@@ -0,0 +1,48 @@
+#! /usr/bin/python
+
+import sys
+
+def print_msg(way, text):
+  hexa = text.split(' ')
+  code = int(hexa[0], 16)
+  length = int(hexa[1], 16)
+  msg = ''
+  for i in range(0, length):
+    n = int(hexa[i+2], 16)
+    if (n == 10):
+      msg += "\\n"
+    elif (n == 13):
+      msg += "\\r"
+    elif ((n >= 32 and n <= 126)):
+      msg += chr(n)
+    else:
+      msg += hex(n)
+  print('%s: code=%s, msg="%s"' % (way, hex(code), msg))
+
+def parse(filename):
+  with open(filename) as f:
+    buff = ['', '', '', '']
+    way = ''
+    for line in f:
+      line = line.strip()
+
+      if 'going down' in line:
+        way = 'Sent'
+      elif 'coming back' in line:
+        way = 'Received'
+      elif line.startswith('00000000:'):
+        buff[0] = line[10:]
+      elif line.startswith('00000010:'):
+        buff[1] = line[10:]
+      elif line.startswith('00000020:'):
+        buff[2] = line[10:]
+      elif line.startswith('00000030:'):
+        buff[3] = line[10:]
+        print_msg(way, ' '.join(buff))
+
+if __name__ == "__main__":
+  if len(sys.argv) != 2:
+    print("Usage: %s <file>" % sys.argv[0])
+  else:
+    parse(sys.argv[1])
+
